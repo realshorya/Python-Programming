2b4bec17cd9df63fb1737c7e2182d297b1070c49
SHORYA <chandrashorya@gmail.com>
2025-08-04 22:30:44 +0530

    Enter Two Numbers and perform all Arithmetic operations

---
+++ arithmetic.py
@@ -0,0 +1,14 @@
+n1=int(input("Enter First number: "))
+n2=int(input("Enter Second Number: "))
+res=n1+n2
+print("Addition is",res)
+res=n1-n2
+print("Subtraction is",res)
+res=n1*n2
+print("Multiplication is",res)
+res=n1/n2
+print("Division is",res)
+res=n1//n2
+print("Quotient is",res)
+res=n1%n2
+print("Remainder is",res)
